65a24b8258559c369cdbdd9623167a43e6b96531
Victoria <turcheniuk.v@gmail.com>
2024-06-07 16:01:06 +0300

    new features
    Signed-off-by: Victoria <turcheniuk.v@gmail.com>

--- js/grayscale.js
+++ js/grayscale.js
@@ -12,6 +12,8 @@ $(window).scroll(collapseNavbar);
 $(document).ready(collapseNavbar);
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
+
+
 $(function() {
     $('a.page-scroll').bind('click', function(event) {
         event.preventDefault();
@@ -22,3 +24,29 @@ $(function() {
         // bsCollapse.toggle();
     });
 });
+
+
+
+$(document).ready(function() {
+    var front = document.getElementsByClassName("front");
+    var back = document.getElementsByClassName("back");
+  
+    var highest = 0;
+    var absoluteSide = "";
+  
+    for (var i = 0; i < front.length; i++) {
+      if (front[i].offsetHeight > back[i].offsetHeight) {
+        if (front[i].offsetHeight > highest) {
+          highest = front[i].offsetHeight;
+          absoluteSide = ".front";
+        }
+      } else if (back[i].offsetHeight > highest) {
+        highest = back[i].offsetHeight;
+        absoluteSide = ".back";
+      }
+    }
+    $(".front").css("height", highest);
+    $(".back").css("height", highest);
+    $(absoluteSide).css("position", "absolute");
+  });
+  
